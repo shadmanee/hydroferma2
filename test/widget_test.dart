3f220d57e713e2a37ac5762546d04d2590a0e0fc
TasfiaSarah <tasfiasara1000@gmail.com>
2022-12-04 16:05:51 +0600

    myapp to loginsignup
    4/12
    4.05pm

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hydroferma2/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LoginSignup());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
